e8b48c80fb3a40e3729b42a2959af43eccceca68
Fiodar Sazanavets <33926695+fiodarsazanavets@users.noreply.github.com>
2023-03-03 18:48:37 +0800

    Refactor for an exercise.

--- index.js
+++ index.js
@@ -2,35 +2,24 @@ const ListAggregate = require('./listAggregate.js');
 const SortedBinaryTreeCollection = require('./sortedBinaryTreeCollection.js');
 
 var listOfValues = [8, 19, 25, 2, 4, 7, 32, 90, 3, 1];
-var listAggregate = new ListAggregate();
 
-for (var i = 0; i < listOfValues.length; i++) {
-    listAggregate.insert(listOfValues[i]);
-}
+var currentIndex = 0;
+
 
 var listIterator = listAggregate.createIterator();
 
 var message = 'Values from List Iterator: ';
 
-while (listIterator.moveNext()) {
-    message += listIterator.getCurrent() + ', ';
+while (true) {
+
+    if (currentIndex + 1 < listOfValues.count()) {
+        currentIndex++;
+        break;
+    }
+
+    message += listOfValues.get(currentIndex);
 }
 
 console.log(message);
 
-var treeAggregate = new SortedBinaryTreeCollection();
-
-for (var i = 0; i < listOfValues.length; i++) {
-    treeAggregate.insert(listOfValues[i]);
-}
-
-var treeIterator = treeAggregate.createIterator();
-
-console.log('');
-message = 'Values from Binary Tree Iterator: ';
-
-while (treeIterator.moveNext()) {
-    message += treeIterator.getCurrent() + ', ';
-}
-
-console.log(message);
+// Implement binary tree iterator here
